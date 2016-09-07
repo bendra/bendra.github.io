e7fda94ad0438c7250cc65459befaa5f3ac8e2c2
Benjamin Drasin <livinglaser@hotmail.com>
2016-09-07 14:47:25 -0700

    Update snappy.js

--- lightning/snappy.js
+++ lightning/snappy.js
@@ -5,10 +5,10 @@ var initESW = function(gslbBaseUrl) {
 	embedded_svc.settings.smallCompanyLogoImgURL = 'https://esw1234.github.io/smallCompanyLogoImg.png';
 	embedded_svc.settings.targetElement = "button_goes_here";
 	
-	embedded_svc.init('http://bdrasin-ltm2.internal.salesforce.com:6109',
-			'http://bdrasin-ltm2.internal.salesforce.com:8096/chat',
-			'https://bdrasin-ltm2.internal.salesforce.com:8095/content',
-			'https://bdrasin-ltm-comm.localhost.force.com:6101/t3c1',
-			'572xx0000000001', '00Dxx0000001gEg', '573xx0000000001', '204.0',
-			gslbBaseUrl);
+	embedded_svc.init('https://na1-blitz01.soma.salesforce.com',
+			'https://d.la-blitz01.soma.salesforce.com/chat',
+			'https://c.la-blitz01.soma.salesforce.com/content',
+			'https://fitbit-1475b6fa058-1550a73a5d9.blitz01.soma.force.com/igorKokua',
+			'572D00000004CUv', '00DD00000008FQY', '573D00000004Ceq', '204.0',
+			null);
 }
